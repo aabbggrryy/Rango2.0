5fc5ff7306fd0d43ae9d2e0dc0464af85d1ef31d
Alexander Bell <2914823B@student.gla.ac.uk>
2025-02-11 13:01:47 +0000

    i am losing my mind. one test is failing and it says that its the new colums created. it looks exactly like the screenshot and i can figure out what is wrong. i am moving on

--- populate_rango.py
+++ populate_rango.py
@@ -9,7 +9,7 @@ def populate():
 
     python_pages = [
         {'title' : 'Official Python Tutorial',
-         'url' : 'http://python.org/3/tuorial/'},
+         'url' : 'http://python.org/3/tutorial/'},
         {'title': 'How to Think like a Computer Scientist',
          'url': 'http://www.greenteapress.com/thinkpython/'},
         {'title': 'Learn Python in 10 Minutes',
@@ -37,9 +37,9 @@ def populate():
             'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16},}
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], views=0)
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
@@ -53,7 +53,9 @@ def add_page(cat, title, url, views=0):
     return p
 
 def add_cat(name, views=0, likes=0):
-    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
+    c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
